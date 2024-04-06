65b1488e90bc89b109e887ff3af57e081f7422cc
Leo123457 <75062444+Leo123457@users.noreply.github.com>
2024-04-06 21:16:39 +0800

    Refactor using classes and methods

--- 65.cc
+++ 65.cc
@@ -6,54 +6,132 @@
 
 using namespace std;
 
-string find(string s, vector<vector<string>> a) {
-    for (vector<string> i : a) {
-        if (i[0] == s) {
-            return i[1];
+enum Type { NUM,
+            ADD,
+            SUB,
+            MUL,
+            DIV };
+
+class Item {
+    int value;
+    Type type;
+
+  public:
+    Item(const int v = 0, const Type t = NUM) : value{v}, type{t} {}
+    Item(const string &s) {
+        if (s == "+") {
+            type = ADD;
+        } else if (s == "-") {
+            type = SUB;
+        } else if (s == "*") {
+            type = MUL;
+        } else if (s == "/") {
+            type = DIV;
+        } else {
+            type = NUM;
+            value = stoi(s);
         }
     }
-}
+    int getValue() const { return value; }
+    Item arithmetic(const Item &x, const Item &b) {
+        switch (type) {
+            case ADD:
+                return Item(x.getValue() + b.getValue());
+            case SUB:
+                return Item(x.getValue() - b.getValue());
+            case MUL:
+                return Item(x.getValue() * b.getValue());
+            case DIV:
+                return Item(x.getValue() / b.getValue());
+            default:
+                cout << "invalid arithmetic\n";
+                exit(-1);
+        }
+    }
+};
+
+class Variable {
+    string name;
+    int value;
+
+  public:
+    Variable(const string n = "", const int v = 0) : name{n}, value{v} {}
+    string getName() const { return name; }
+    int getValue() const { return value; }
+    void setValue(const int v) { value = v; }
+    static bool isVariable(const string &s) {
+        for (auto i : s) {
+            if (!isalpha(i) && i != '_' && i != '$' && !isdigit(i)) {
+                return false;
+            }
+        }
+        return !isdigit(s[0]);
+    }
+};
+
+class VariableSet {
+    vector<Variable> variables;
+
+  public:
+    VariableSet() {
+        string name;
+        char equal;
+        int value;
+        while (cin >> name >> equal >> value) {
+            variables.push_back(Variable(name, value));
+        }
+    }
+    Item find(const string n) const {
+        for (auto &variable : variables) {
+            if (variable.getName() == n) {
+                return Item(variable.getValue());
+            }
+        }
+        cout << "variable not found\n";
+        exit(-1);
+    }
+};
+
+class Statements {
+    stringstream statements;
+
+  public:
+    Statements() {
+        string s;
+        getline(cin, s);
+        statements = stringstream(s);
+    }
+    void run(VariableSet &v) {
+        stack<Item> s;
+        string item;
+        while (statements >> item) {
+            if (item == "(") {
+                continue;
+            }
+            Item o, x, y;
+            if (item == ")") {
+                y = s.top();
+                s.pop();
+                x = s.top();
+                s.pop();
+                o = s.top();
+                s.pop();
+                s.push(o.arithmetic(x, y));
+            } else {
+                if (Variable::isVariable(item)) {
+                    s.push(v.find(item));
+                } else {
+                    s.push(Item(item));
+                }
+            }
+        }
+        cout << s.top().getValue() << endl;
+    }
+};
 
 int main() {
-    vector<vector<string>> a;
-    string b, v, x, y, o;
-
-    getline(cin, b);
-    stringstream ss(b);
-    while (cin >> x >> o >> y) {
-        a.push_back({x, y});
-    }
-    stack<string> s;
-    string p;
-    while (ss >> p) {
-        if (p == "(") {
-            continue;
-        } else if (p == ")") {
-            y = s.top();
-            s.pop();
-            if (y[0] != '-' && (y[0] < '0' || y[0] > '9')) {
-                y = find(y, a);
-            }
-            x = s.top();
-            s.pop();
-            if (x[0] != '-' && (x[0] < '0' || x[0] > '9')) {
-                x = find(x, a);
-            }
-            v = s.top();
-            s.pop();
-            if (v == "+") {
-                s.push(to_string(stoi(x) + stoi(y)));
-            } else if (v == "-") {
-                s.push(to_string(stoi(x) - stoi(y)));
-            } else if (v == "*") {
-                s.push(to_string(stoi(x) * stoi(y)));
-            } else if (v == "/") {
-                s.push(to_string(stoi(x) / stoi(y)));
-            }
-        } else {
-            s.push(p);
-        }
-    }
-    cout << s.top() << '\n';
+    Statements statements;
+    VariableSet v;
+    statements.run(v);
     return 0;
 }
